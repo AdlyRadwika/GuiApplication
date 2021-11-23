995ea1c9850f19019e8b9347aff71bc77fb72288
AdlyRadwika <aradwika@gmail.com>
2021-11-23 18:43:34 +0700

    Fitur pada layout dashboard

--- app/src/main/java/com/example/guiappadlyr/Dashboard.java
+++ app/src/main/java/com/example/guiappadlyr/Dashboard.java
@@ -2,7 +2,9 @@ package com.example.guiappadlyr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+
+    public void openRegis(View v){
+
+        Intent intent = new Intent(Dashboard.this, Register.class);
+        startActivity(intent);
+
+    }
+
+    public void openMoney(View v){
+
+        Intent intent = new Intent(Dashboard.this, Conversion.class);
+        startActivity(intent);
+
+    }
+
+    public void backtoMain(View v){
+
+        Intent intent = new Intent(Dashboard.this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
