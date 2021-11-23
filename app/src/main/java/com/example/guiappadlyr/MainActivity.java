8df3087bd4e09731d704cc92cfc3f5680d160923
AdlyRadwika <aradwika@gmail.com>
2021-11-23 10:50:28 +0700

    Perpindahan antar layout

--- app/src/main/java/com/example/guiappadlyr/MainActivity.java
+++ app/src/main/java/com/example/guiappadlyr/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.guiappadlyr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showMoney(View v){
+        Intent intent = new Intent(MainActivity.this, Conversion.class);
+        startActivity(intent);
+    }
+
+    public void showRegister(View v){
+        Intent intent = new Intent(MainActivity.this, Register.class);
+        startActivity(intent);
+    }
+
+    public void showDashboard(View v){
+        Intent intent = new Intent(MainActivity.this, Dashboard.class);
+        startActivity(intent);
     }
 }
